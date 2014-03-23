d4815a5ae706f68766d8649ea55d194b82ffc4d2
Vincenzo Prignano <vincenzo.prignano@gmail.com>
2014-03-23 11:08:24 +0100

    update

--- chatapp.py
+++ chatapp.py
@@ -43,4 +43,5 @@ def handle_message(data):
 
 
 if __name__ == '__main__':
-    ws.run(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 3001))
+    ws.run(app, host='0.0.0.0', port=port)
